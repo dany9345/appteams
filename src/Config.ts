ec86cf57a6b1e7a478aacb8f41999a5d840b9c89
eneko rodriguez external <eneko.rodriguez.external@atos.net>
2021-04-21 10:04:32 +0200

    Initial commit

---
+++ src/Config.ts
@@ -0,0 +1,9 @@
+export const config = {
+    appId: 'd7e011f5-939e-4ca2-b2e6-be6a704cb460',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+      'user.read',
+      'mailboxsettings.read',
+      'calendars.readwrite'
+    ]
+  };
